095ab23abfc40d46e7f452e6442bae7386cb8fdc
alecgn <alecgn@hotmail.com>
2024-11-21 16:01:16 -0300

    Update Program.cs

--- samples/ConsoleApp_Net8/Program.cs
+++ samples/ConsoleApp_Net8/Program.cs
@@ -6,6 +6,8 @@
         {
             AleCGN.Security.Cryptography.Encoders.IEncoder base64Encoder = new AleCGN.Security.Cryptography.Encoders.Base64Encoder();
             AleCGN.Security.Cryptography.ISymmetricKeyHelper symmetricKeyHelper = new AleCGN.Security.Cryptography.SymmetricKeyHelper(base64Encoder);
+            // string base64Encoded256bitKey = symmetricKeyHelper.GenerateSecureRandom256BitEncodedKey(); // you should store the key as a base64 or hexadecimal encoded string for later use
+            // AesGcmBase implementations, like AesGcm256, supports an encoded key in constructor overloads
             byte[] key256bit = symmetricKeyHelper.GenerateSecureRandom256BitKey();
             AleCGN.Security.Cryptography.Encryption.Algorithms.Aes.IAesGcm256 aesGcm256 = new AleCGN.Security.Cryptography.Encryption.Algorithms.Aes.AesGcm256(base64Encoder, key256bit);
 
